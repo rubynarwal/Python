7b8c4e4c3864375ddaa1a321c65047201e2026f8
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-12-31 12:55:53 +0530

    ruby

---
+++ q8.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+# load image
+image = Image.open("diwali.jpg")
+
+# show image
+image.show()
+
+# print message
+print("Happy Diwali! May the festival of lights bring happiness, prosperity, and peace to you and your loved ones.")
---
+++ q9.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+# load image
+image = Image.open("mother.jpg")
+
+# show image
+image.show()
+
+# print message
+print("Happy Mother's Day! Thank you for all that you do. We love you.")
