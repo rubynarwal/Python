255b9318cfb0532547bc960a6376b64e791c05fd
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-12-31 13:00:21 +0530

    ruby

---
+++ q11.py
@@ -0,0 +1,20 @@
+def calculate(num1, num2, operator):
+  if operator == "+":
+    return num1 + num2
+  elif operator == "-":
+    return num1 - num2
+  elif operator == "*":
+    return num1 * num2
+  elif operator == "/":
+    return num1 / num2
+  else:
+    return "Invalid operator"
+
+# get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+
+# perform the calculation and print the result
+result = calculate(num1, num2, operator)
+print(result)
