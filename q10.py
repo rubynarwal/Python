f26c89dc84264841614cc5993d67fcce37bca318
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-12-31 12:57:02 +0530

    ruby

---
+++ q10.py
@@ -0,0 +1,11 @@
+from PIL import Image
+
+# load image
+image = Image.open("sister.jpg")
+
+# show image
+image.show()
+
+# print message
+print("Happy Sister's Day! Thank you for always being there for me. I love you.")
+This code fir
